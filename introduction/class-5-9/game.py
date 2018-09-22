38e6f349e00c889ef346f35d3b4acaf466c14e53
Anselmo Pfeifer <apfeifer@daitangroup.com>
2018-09-22 01:25:31 -0400

    Added new folder for introduction module

--- introduction/class-5-9/game.py
+++ introduction/class-5-9/game.py
@@ -1,18 +1,17 @@
 import random
-from config import logs
 
-LOG = logs.logger
+from introduction.utils import logger
 
-LOG.info('-'*50)
-LOG.info('- This is the Python3 Game, Welcome')
-LOG.info('-'*50)
+logger.info('-'*50)
+logger.info('- This is the Python3 Game, Welcome')
+logger.info('-'*50)
 
 attempts = 1
 secret_number = random.randrange(1, 101)
 
 
 def get_level():
-    LOG.info('What level do you want? (1=Easy, 2=Medium, 3=Strong)')
+    logger.info('What level do you want? (1=Easy, 2=Medium, 3=Strong)')
     all_attempts = 0
     game_level = int(input())
 
@@ -26,7 +25,7 @@ def get_level():
         all_attempts = 5
 
     else:
-        LOG.error('Use the 1, 2 ou 3 on level game!')
+        logger.error('Use the 1, 2 ou 3 on level game!')
 
     return all_attempts
 
@@ -36,10 +35,10 @@ def execute_game(level):
     points = 1000
 
     for attempts in range(1, level + 1):
-        LOG.info('You have attempts {} de {}: '.format(attempts, level))
+        logger.info('You have attempts {} de {}: '.format(attempts, level))
 
         kick_str = input('Type the number!\n')
-        LOG.info('You type the number {}'.format(kick_str))
+        logger.info('You type the number {}'.format(kick_str))
 
         kick = int(kick_str)
         right = kick == secret_number
@@ -47,34 +46,30 @@ def execute_game(level):
         less = kick < secret_number
 
         if kick < 1:
-            LOG.error('You must type number higher 1 and less 100!!!')
+            logger.error('You must type number higher 1 and less 100!!!')
             continue
 
         if right:
             game_result = True
-            LOG.info('='*50)
-            LOG.info('Congratulation you have {:7.2f} points!!!'.format(points))
-            LOG.info('='*50)
+            logger.info('='*50)
+            logger.info('Congratulation you have {:7.2f} points!!!'.format(points))
+            logger.info('='*50)
             break
         else:
             if bigger:
-                LOG.info('You are wrong! The kick was higher than Secret number!!!')
+                logger.info('You are wrong! The kick was higher than Secret number!!!')
             elif less:
-                LOG.info('You are wrong! The kick was less than Secret number!!!')
+                logger.info('You are wrong! The kick was less than Secret number!!!')
             lost_points = round(abs(secret_number - kick) /3) # 40 -20 = 20
             points = points - lost_points
 
         attempts += 1
 
     if not game_result:
-        LOG.warning('='*25)
-        LOG.warning('Gamer Over!!!')
-        LOG.warning('='*25)
-
-
-def main():
-    execute_game(get_level())
+        logger.warning('='*25)
+        logger.warning('Gamer Over!!!')
+        logger.warning('='*25)
 
 
 if __name__ == '__main__':
-    main()
+    execute_game(get_level())
