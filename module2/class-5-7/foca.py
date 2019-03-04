80ad9c1f0684f9cbdaaed393d6cc07f43ad0e1ff
Anselmo Pfeifer <apfeifer@daitan.com>
2019-03-04 19:59:05 -0400

    Re-factory

--- module2/class-5-7/foca.py
+++ module2/class-5-7/foca.py
@@ -9,13 +9,31 @@ def start_message():
 
 
 def end_message(winner):
-    logger.info('-'*50)
+    lost = """
+                ______________
+              /               \      
+             /                 \     
+          //                   \/\ 
+          \|   XXXX     XXXX   | /  
+            |   XXXX     XXXX   |/ 
+            |   XXX       XXX   |   
+            |                   |  
+             \__      XXX      __/     
+              |\     XXX     /|       
+              | |           | |        
+              | I I I I I I I |        
+              |  I I I I I I  |       
+              \_             _/       
+               \_         _/         
+                 \_______/           
+    """
     logger.info('The game is finished!!!')
     if winner:
-        logger.info('Congratulation, you are the winner!!')
+        logger.info('Congratulation, you are the winner!!!')
     else:
-        logger.warn('OH MY GOOD, you are not winner!!')
-    logger.info('-'*50)
+        logger.error('OH MY GOOD, you are not winner!!')
+        logger.error(lost)
+
 
 
 def read_file():
@@ -34,23 +52,23 @@ def hidden_letter(word):
     return ['_' for letter in word]
 
 
-def start_game(secret_word):
-    the_correct_word = hidden_letter(secret_word)
+def start_game(word):
+    the_correct_word = hidden_letter(word)
 
     hang_himself = False
     winner = False
     errors = 0
 
     print('The secret word {} with size: {}'.format(the_correct_word, len(the_correct_word)))
-    logger.warn(secret_word)
+    logger.warn(word)
 
     while not hang_himself and not winner:
         option = input('What is the word?\n')
         option = option.strip().upper()
 
-        if option in secret_word:
+        if option in word:
             index = 0
-            for letter in secret_word:
+            for letter in word:
                 if option == letter:
                     the_correct_word[index] = option
                 index += 1
